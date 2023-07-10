792ed1518e41b1c48d5c8c625fa4e24715ae9b3c
DanielAmbro <DanielAmbro1@outlook.com>
2023-07-10 18:40:24 -0500

    Add project files.

---
+++ Multiples3sAnd5s/Challenge1_Multiple3sAnd5s.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Multiples3sAnd5s
+{
+    internal class Challenge1_Multiple3sAnd5s
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello there, are you looking for the sum of all the multiples of 3 and 5?");
+            int sum = 0;
+            for (int i = 1; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine("You're in Luck! The answer is :" + sum);
+        } 
+    }
+}
